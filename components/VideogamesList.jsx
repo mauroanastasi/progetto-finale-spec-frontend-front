cca02c4ea11169006447bb452589a5bb20c8a1ef
mauroanastasi <mauroanastasi1@gmail.com>
2025-09-30 02:47:10 +0200

    [UPD] fetch test videogames list

--- components/VideogamesList.jsx
+++ components/VideogamesList.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const VideogamesList = () => {
+
+    const apiUrl = import.meta.env.VITE_API_URL
+
+    const [videogames, setVideogames] = useState([])
+
+
+    useEffect(() => {
+        fetch(`${apiUrl}/videogames`)
+            .then(resp => {
+                if (!resp.ok) {
+                    throw new Error(`HTTP error! Status: ${resp.status}`);
+                }
+                return resp.json();
+            })
+            .then(data => {
+                console.log("Videogiochi caricati:", data);
+                setVideogames(data);
+            })
+            .catch(err => console.error('Fetch error:', err));
+    }, []);
+
+
+    useEffect(() => {
+        console.log(videogames)
+    }, [videogames])
+    console.log("API URL:", apiUrl)
     return (
         <div>
+            <h1>Videogames</h1>
+            <ul>
+                {videogames.map((game) => (
+                    <li key={game.id}>
+                        <h2>{game.title}</h2>
+                        <p>{game.category}</p>
+                    </li>
 
-        </div>
+
+                ))}
+            </ul>
+        </div >
     )
 }
 
